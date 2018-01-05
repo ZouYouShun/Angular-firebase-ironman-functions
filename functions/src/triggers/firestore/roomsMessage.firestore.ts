1c7241fdd96bb4a93358d8afef388773bcd863d3
AlanZou <e936106@gmail.com>
2018-01-05 17:32:48 +0800

    add roomsMessage.firestore.ts

--- functions/src/triggers/firestore/roomsMessage.firestore.ts
+++ functions/src/triggers/firestore/roomsMessage.firestore.ts
@@ -2,8 +2,9 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import { storeTimeObject } from '../../libs/timestamp';
 
+// 當訊息有資料寫入時觸發
 export const roomsMessagefirestore = functions.firestore
-  .document('/rooms/{roomId}/messages/{messageId}').onWrite((event) => {
+  .document('/rooms/{roomId}/messages/{messageId}').onCreate((event) => {
     const firestore = admin.firestore();
 
     const roomId = event.params.roomId;
@@ -11,7 +12,18 @@ export const roomsMessagefirestore = functions.firestore
 
     const message = event.data.data();
 
-    const messageRef: FirebaseFirestore.DocumentReference = event.data.ref;
-
-    return messageRef.parent.parent.update(storeTimeObject({ last: message }, false));
+    return Promise.all([
+      // 更新這個人對應到另一個人的最後一句資料
+      firestore.doc(`users/${message.sender}`)
+        .collection('rooms')
+        .doc(message.addressee)
+        .update({ last: message }),// because data contain createdAt and updatedAt, no longer to write again
+      // 兩個人的都要更新
+      firestore.doc(`users/${message.addressee}`)
+        .collection('rooms')
+        .doc(message.sender)
+        .update({ last: message }),
+    ])
+    // const messageRef: FirebaseFirestore.DocumentReference = event.data.ref;
+    // return messageRef.parent.parent.update(storeTimeObject({ last: message }, false));
   });
