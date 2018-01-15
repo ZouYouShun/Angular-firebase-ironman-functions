cd0c76713c560220e18e33f3fb719e48998abc1c
AlanZou <e936106@gmail.com>
2018-01-15 23:16:54 +0800

    add file resize by image-size

--- functions/src/triggers/storage/generateThumbnail.storage.ts
+++ functions/src/triggers/storage/generateThumbnail.storage.ts
@@ -3,6 +3,7 @@ import * as cpp from 'child-process-promise';
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import * as path from 'path';
+import * as sizeOf from 'image-size';
 
 import { storeTimeObject } from '../../libs/timestamp';
 import { CONFIG } from '../../config';
@@ -45,48 +46,64 @@ export const generateThumbnail = functions.storage.object()
     // 下載原檔
     return bucket.file(filePath).download({
       destination: tempFilePath
-    })
-      .then(() => {
-        // 下載完成後，我們執行spawn來呼叫Google Cloud提供的縮圖功能
-        console.log('圖片下載完成，在', tempFilePath);
-        return spawn('convert', [tempFilePath, '-thumbnail', '200x200',
-          tempFilePath])
-      }).then(() => {
-        // 縮圖產生完成後，把縮圖透過bucket上傳到storage
-        console.log('縮圖產生完成');
+    }).then(async () => {
+      const img = sizeOf(tempFilePath);
 
-        return bucket.upload(tempFilePath, {
-          destination: thumbFilePath,
-          metadata: { // 這裡要注意，我們的metadata
-            metadata: { // customMetada放這裡
-              complete: true // 我們新增一個complete的屬性
-            }
-          }
-        });
-      }).then(() => {
-        const config = {
-          action: 'read',
-          expires: '08-03-2491' // I don't want to expire
+      let width = img.width;
+      let height = img.height;
+      if (width > 200 || height > 200) {
+        const rate = img.height / img.width;
+
+        // 高比較大，固定高
+        if (rate > 1) {
+          height = 200;
+          width = 200 / rate;
+        } else { // 寬比較大，固定寬
+          width = 200;
+          height = 200 * rate;
         }
-        // get files download url
-        return Promise.all([
-          bucket.file(thumbFilePath).getSignedUrl(config),
-          bucket.file(filePath).getSignedUrl(config)
-        ])
-      }).then(([thumbResult, originalResult]) => {
-        const url = originalResult[0]; // 注意這裡回傳的是陣列
-        const thumbnail = thumbResult[0]; // 注意這裡回傳的是陣列
-        return filesRef.doc(encodePath)
-          .set(storeTimeObject({
-            path: filePath,
-            contentType: object.contentType,
-            creator: metadata.creator || 'system',
-            updater: metadata.updater || 'system',
-            url,
-            thumbnail,
-          }))
-      }).catch((err) => {
-        console.error(err);
-        return err;
+      }
+
+      // 下載完成後，我們執行spawn來呼叫Google Cloud提供的縮圖功能
+      console.log('圖片下載完成，在', tempFilePath);
+      return spawn('convert', [tempFilePath, '-thumbnail', `${width}x${height}`,
+        tempFilePath])
+    }).then(() => {
+      // 縮圖產生完成後，把縮圖透過bucket上傳到storage
+      console.log('縮圖產生完成');
+
+      return bucket.upload(tempFilePath, {
+        destination: thumbFilePath,
+        metadata: { // 這裡要注意，我們的metadata
+          metadata: { // customMetada放這裡
+            complete: true // 我們新增一個complete的屬性
+          }
+        }
       });
+    }).then(() => {
+      const config = {
+        action: 'read',
+        expires: '08-03-2491' // I don't want to expire
+      }
+      // get files download url
+      return Promise.all([
+        bucket.file(thumbFilePath).getSignedUrl(config),
+        bucket.file(filePath).getSignedUrl(config)
+      ])
+    }).then(([thumbResult, originalResult]) => {
+      const url = originalResult[0]; // 注意這裡回傳的是陣列
+      const thumbnail = thumbResult[0]; // 注意這裡回傳的是陣列
+      return filesRef.doc(encodePath)
+        .set(storeTimeObject({
+          path: filePath,
+          contentType: object.contentType,
+          creator: metadata.creator || 'system',
+          updater: metadata.updater || 'system',
+          url,
+          thumbnail,
+        }))
+    }).catch((err) => {
+      console.error(err);
+      return err;
+    });
   });
