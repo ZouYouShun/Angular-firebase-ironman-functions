75fb86d3463a2506cc72867ee50c844578b1144b
AlanZou <e936106@gmail.com>
2018-01-17 02:17:27 +0800

    fix loginstatus to user's own data

--- functions/src/triggers/realtimeDatabase/user-status.realtime.trigger.ts
+++ functions/src/triggers/realtimeDatabase/user-status.realtime.trigger.ts
@@ -6,7 +6,7 @@ export const userStatusFirestore = functions.database
     const firestore = admin.firestore();
     const eventStatus = event.data.val();
 
-    const userStatusFirestoreRef = firestore.doc(`status/${event.params.uid}`);
+    const userRef = firestore.doc(`users/${event.params.uid}`);
 
     // 資料可能快速的被做修改，如果我們發現時間事件時間小於資料庫的更新時間，不做處理
     return event.data.ref.once("value").then((statusSnapshot) => {
@@ -15,7 +15,9 @@ export const userStatusFirestore = functions.database
       if (status.updatedAt > eventStatus.updatedAt) return null;
 
       // 把資料轉乘時間格式
-      eventStatus.updatedAt = new Date(eventStatus.updatedAt);
-      return userStatusFirestoreRef.set(eventStatus);
+      return userRef.update({
+        loginStatus: status.state,
+        lastSignInTime: new Date(eventStatus.updatedAt)
+      });
     });
   });
